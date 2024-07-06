1dcaf43ed0528e0ca9bf7f4ce35e588e94e35e1d
Dayanand-MK <dayanandmanikandan@yahoo.com>
2024-07-06 22:47:36 +0530

    3.py

--- TASK 1 - BASIC PYTHON SYNTAX UNDERSTANDING (5.7.24)/3.py
+++ TASK 1 - BASIC PYTHON SYNTAX UNDERSTANDING (5.7.24)/3.py
@@ -1,35 +1,35 @@
 # GAIN FAMILIARITY WITH COMMON DATA STRUCTURES LIKE LIST, DICTIONARIES, AND TUPLES.
 
 def list():
-    li = [5,15,17,20,25,30]
+    li = [5,15,17,20,25,30]                             #list
     print("List : ",li)
-    li.append(35)
-    li.insert(1,10)
-    li.remove(17)
-    a = li[1]
+    li.append(35)                                       #append
+    li.insert(1,10)                                     #insert
+    li.remove(17)                                       #remove
+    a = li[1]                                           #index
     print("At index 1 : ",a)
     print("Updated list : ",li)
 
 def dict():
-    d = {"Name" : "Dayanand","Age" : 17,"Year" : 1}
+    d = {"Name" : "Dayanand","Age" : 17,"Year" : 1}     #dictionary
     print("Dictionary : ",d)
-    d["Dept"] = "AI&ML"
-    del d["Year"]
-    d["Age"] = 18
+    d["Dept"] = "AI&ML"                                 #insert
+    del d["Year"]                                       #delete
+    d["Age"] = 18                                       #update
     print("Updated Dictionary : ",d)
 
 def tuple():
-    t = (5,15,17,20,25,30)
-    u = t + (35,)
-    t[:1]+t[5:]
+    t = (5,15,17,20,25,30)                              #tuple
+    u = t + (35,)                                       #append
+    t[:1]+t[5:]                                         #remove
     print("Removed Tuple : ",t)
     l = 10
-    t[:1]+(l,)+t[6:]
+    t[:1]+(l,)+t[6:]                                    #update
     print("Updated Tuple : ",t)
 
 print("LIST")
-list()
+list()                                                  #call function
 print("DICTIONARY")
-dict()
+dict()                                                  #call function
 print("TUPLE")
-tuple()
+tuple()                                                 #call function
